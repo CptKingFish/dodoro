ffa3f7dfa2471341221d711ce49f902f9ec6d1a3
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-10-02 20:19:27 +0800

    f

--- src/components/blog_cards_lg.jsx
+++ src/components/blog_cards_lg.jsx
@@ -29,7 +29,7 @@ export default function BlogCardsLg() {
             Learn more about our causes.
           </h2>
         </div>
-        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-1">
           {posts.map((post) => (
             <article
               key={post.id}
--- src/pages/blog/dodol.jsx
+++ src/pages/blog/dodol.jsx
@@ -1,6 +1,14 @@
 import Footer from "@/components/footer";
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import { CameraIcon } from "@heroicons/react/20/solid";
+import {
+  CloudArrowUpIcon,
+  FireIcon,
+  HandRaisedIcon,
+  LockClosedIcon,
+  MoonIcon,
+  ServerIcon,
+} from "@heroicons/react/20/solid";
 
 export default function BlogDodol() {
   return (
@@ -11,7 +19,7 @@ export default function BlogDodol() {
           <div className="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
             <div>
               <h2 className="text-lg font-semibold text-yellow-500">Article</h2>
-              <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-yellow-900 sm:text-4xl">
+              <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-yellow-950 sm:text-4xl">
                 The Dodol makers of Tanete village
               </h3>
             </div>
@@ -73,72 +81,97 @@ export default function BlogDodol() {
               </div>
             </div>
             <div className="mt-8 lg:mt-0">
-              <p className="mt-6 text-xl leading-8">
-                Aliquet nec orci mattis amet quisque ullamcorper neque, nibh
-                sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque
-                id at vitae feugiat egestas ac. Diam nulla orci at in viverra
-                scelerisque eget. Eleifend egestas fringilla sapien.
+              <span className="mt-2 text-2xl font-bold tracking-tight text-yellow-900">
+                What is Dodol?
+              </span>
+              <p className="mt-6 text-lg leading-8 text-gray-700">
+                Dodol, also known as Dodoro in Makassar, is a traditional
+                confectionery originating from Indonesia, composed of sugar
+                palm, white and black glutinous rice, palm sugar, and coconut
+                milk. This confection holds significant cultural importance in
+                Indonesia, often gracing the tables of weddings and festivals.
               </p>
-              <div className="mt-10 max-w-2xl">
-                <p>
-                  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                  risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                  Id dolor praesent donec est. Odio penatibus risus viverra
-                  tellus varius sit neque erat velit. Faucibus commodo massa
-                  rhoncus, volutpat. Dignissim sed eget risus enim. Mattis
-                  mauris semper sed amet vitae sed turpis id.
+              <div className="mt-10 max-w-xl leading-7 text-gray-700 lg:max-w-none text-lg">
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-yellow-900">
+                  How is Dodol made?
+                </h2>
+                <p className="mt-6">
+                  The production of dodol involves a labor-intensive, manual
+                  process. Dodol makers eschew the use of machinery or
+                  specialized equipment, opting instead for hands-on
+                  craftsmanship.
                 </p>
-                <ul
-                  role="list"
-                  className="mt-8 max-w-xl space-y-8 text-gray-600"
-                >
+                <p className="mt-6">
+                  The cooking procedure itself spans a duration of 2-4 hours,
+                  employing firewood to facilitate the process. A lengthier
+                  cooking time contributes to an extended shelf life for the
+                  final product. Typically, a bag of dodol weighs approximately
+                  50 grams and is retailed for 4000 to 5000 rupiah.
+                </p>
+
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-yellow-900">
+                  The cooking process
+                </h2>
+
+                <ul role="list" className="mt-8 space-y-8 text-gray-900">
                   <li className="flex gap-x-3">
-                    <CheckCircleIcon
-                      className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                    <MoonIcon
+                      className="mt-1 h-5 w-5 flex-none text-yellow-600"
                       aria-hidden="true"
                     />
                     <span>
-                      <strong className="font-semibold text-gray-900">
-                        Data types.
-                      </strong>{" "}
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                      Maiores impedit perferendis suscipit eaque, iste dolor
-                      cupiditate blanditiis ratione.
+                      {/* <strong className="font-semibold text-gray-900">
+                        Push to deploy.
+                      </strong>{" "} */}
+                      The initial stages of cooking entail soaking glutinous
+                      rice overnight, followed by milling it into a fine powder
+                      and sun-drying.
                     </span>
                   </li>
                   <li className="flex gap-x-3">
-                    <CheckCircleIcon
-                      className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                    <HandRaisedIcon
+                      className="mt-1 h-5 w-5 flex-none text-yellow-600"
                       aria-hidden="true"
                     />
                     <span>
-                      <strong className="font-semibold text-gray-900">
-                        Loops.
-                      </strong>{" "}
-                      Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                      irure qui lorem cupidatat commodo.
+                      {/* <strong className="font-semibold text-gray-900">
+                        SSL certificates.
+                      </strong>{" "} */}
+                      The extraction of coconut milk is accomplished manually,
+                      by hand.
                     </span>
                   </li>
                   <li className="flex gap-x-3">
-                    <CheckCircleIcon
-                      className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                    <FireIcon
+                      className="mt-1 h-5 w-5 flex-none text-yellow-600"
                       aria-hidden="true"
                     />
                     <span>
-                      <strong className="font-semibold text-gray-900">
-                        Events.
-                      </strong>{" "}
-                      Ac tincidunt sapien vehicula erat auctor pellentesque
-                      rhoncus. Et magna sit morbi lobortis.
+                      {/* <strong className="font-semibold text-gray-900">
+                        Database backups.
+                      </strong>{" "} */}
+                      Subsequently, all ingredients are combined and set to cook
+                      over a firewood-fueled flame. Dodol makers diligently stir
+                      the mixture for hours on end to prevent scorching.
                     </span>
                   </li>
                 </ul>
-                <p className="mt-8">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien
-                  duis odio id et. Id blandit molestie auctor fermentum
-                  dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
-                  varius vulputate et ultrices hac adipiscing egestas. Iaculis
-                  convallis ac tempor et ut. Ac lorem vel integer orci.
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-yellow-900">
+                  The plight of Dodol makers
+                </h2>
+                <p className="mt-6">
+                  Making Dodol isn&apos;t easy. Dodol makers suffers from back
+                  pain, eye irritation, breathing difficulty and etc from making
+                  Dodol. Their stove is positioned close to the ground where
+                  they would have to bend down while exerting force to stir the
+                  Dodol mixture.
+                </p>
+                <p className="mt-6">
+                  In addition, they would have to stir the concoction for hours
+                  on end in that same awkward position which leads to them
+                  having back ache. As they stir the concoction, the ashy smoke
+                  rising from the wok would hurt their eyes and affect their
+                  work performance as well.
                 </p>
               </div>
             </div>
